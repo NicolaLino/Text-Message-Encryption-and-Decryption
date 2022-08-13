8f26ceef382799e72bc3d5eb85c41982d937cd82
NicolaLino <nicola.abu.shaibeh@gmail.com>
2022-08-13 22:58:06 +0300

    Add comments

--- SimpleEncryption.sh
+++ SimpleEncryption.sh
@@ -1,40 +1,43 @@
 #!/bin/bash
 
 function encryption() {
-    arrsum=()
+    arrsum=() # array to store the sum of characters numbers
 
-    while read line; do
+    while read line; do # loop in every line in the file
 
         for word in $line; do
             sum=0
-            string=$(echo "$word" | tr '\r' ' ' | tr -d '\n' | tr -d ' ')
-            if [[ $string == *[,.'!'@\#\$%^\&*()_+]* ]]; then
+            string=$(echo "$word" | tr '\r' ' ' | tr -d '\n' | tr -d ' ') # remove any spaces or newline in a word
+            if [[ $string == *[,.'!'@\#\$%^\&*()_+]* ]]; then             # check for special characters in the file and return an error in exist
                 printf "\n"
                 echo "$(tput setaf 1)"ERROR: THERE ARE SPECIAL CHARACTERS IN THE FILE!""
                 printf "\n"
                 exit 1
                 return 1
             fi
+            #loop through every letter in a word
             while [[ $string ]]; do
-                printf -v letter2number '%d' "$((36#${string::1} - 9))"
-                sum=$((($sum + $letter2number) % 256))
-                string=${string:1}
+                printf -v letter2number '%d' "$((36#${string::1} - 9))" # converting every letter to number using base-36
+                sum=$(($sum + $letter2number))                          # sum of all letters
+                string=${string:1}                                      # increment to next letter in a word
             done
-            arrsum+=("$sum")
+            mod=$(($sum % 256))
+            arrsum+=("$mod") # add the 256 mod of the sum to the array
         done
     done <"$fileName"
 
     # echo "${arrsum[@]}"
+    #find the max number in the array which is the key
     max=${arrsum[0]}
-
     for n in "${arrsum[@]}"; do
         ((n > max)) && max=$n
     done
-
+    # echo "$max"
     printf "\n"
     echo "$(tput setaf 1)"ENCRYPTION KEY WAS GENERATED SUCCESSFULLY"$(tput sgr0)"
     echo "IN DECIMAL==>${max}"
-    binarymax=$(echo "obase=2;$max" | bc)
+    binarymax=$(echo "obase=2;$max" | bc) # convert key from decimal to binary
+    binarymax=$(printf "%08d\n" "$binarymax") # always 8 bits
     echo "IN BINARY==>${binarymax}"
     printf "\n"
     echo "$(tput bold)$(tput setaf 4)"ENTER A FILE NAME YOU WANT TO SAVE CIPHER TEXT INTO"$(tput sgr0)"
@@ -48,15 +51,15 @@ function encryption() {
 
         for word in $line; do
 
-            for ((i = 0; i < ${#word}; i++)); do
+            for ((i = 0; i < ${#word}; i++)); do # loop for each letter in the file
 
-                AscValue=$(echo "${word:$i:1}" | tr -d "\n" | od -An -t dC)
-                BinaryAscValue=$(echo "obase=2;$AscValue" | bc)
+                AscValue=$(echo "${word:$i:1}" | tr -d "\n" | od -An -t dC) # find the ascii value of the letter
+                BinaryAscValue=$(echo "obase=2;$AscValue" | bc)             # convert it to binary
 
                 if [ "$BinaryAscValue" != 1101 ]; then
                     # echo "$(($BinaryAscValue ^ $max))"
-                    xor=$(perl -e 'printf("%.8b",oct("0b".$ARGV[0])^oct("0b".$ARGV[1]))' $binarymax $BinaryAscValue)
-                    echo -e -n "${xor:4:8}${xor:0:4} " >>"$encFile"
+                    xor=$(perl -e 'printf("%.8b",oct("0b".$ARGV[0])^oct("0b".$ARGV[1]))' $binarymax $BinaryAscValue) #XOR key with ascii binary of the letter
+                    echo -e -n "${xor:4:8}${xor:0:4} " >>"$encFile"                                                  # flip first 4 bits with last 4 bits
                 else
                     continue
                 fi
@@ -71,12 +74,12 @@ function encryption() {
         done
     done <"$fileName"
 
-    echo -e -n "${binarymax:4:8}${binarymax:0:4} \n" >>"$encFile"
+    echo -e -n "${binarymax:4:8}${binarymax:0:4} \n" >>"$encFile" # add encryption key to the end of the file after fliping first 4 bits with last 4 bits
     # echo -e "\n"
 }
 
 function decryption() {
-    while true; do
+    while true; do #open cipher file
         echo "$(tput bold)$(tput setaf 4)"ENTER CIPHER TEXT FILE NAME TO DECRYPT -OR- ENTER -1 TO EXIT"$(tput sgr0)"
         printf "==>"
         read -r decFile
@@ -94,13 +97,13 @@ function decryption() {
         break
     done
 
-    while read -r line; do
+    while read -r line; do # fing encryption key
         for word in $line; do
             k=$word
         done
     done <"$decFile"
 
-    key="${k:4:8}${k:0:4}"
+    key="${k:4:8}${k:0:4}" # flip the key bits
     printf "\n"
     echo "$(tput setaf 1)"ENCRYPTION KEY WAS FOUND SUCCESSFULLY"$(tput sgr0)"
     echo "IN BINARY==>${key}"
@@ -113,16 +116,17 @@ function decryption() {
     read -r plainfile
     printf "\n"
 
+    # loop through the file to decrypt
     while read -r l; do
         for w in $l; do
             if [ "${w:4:8}${w:0:4}" != "$key" ]; then
                 if [ "$w" != 00100000 ]; then
                     ascibinary="${w:4:8}${w:0:4}"
-                    xor_2=$(perl -e 'printf("%.8b",oct("0b".$ARGV[0])^oct("0b".$ARGV[1]))' $ascibinary $key)
+                    xor_2=$(perl -e 'printf("%.8b",oct("0b".$ARGV[0])^oct("0b".$ARGV[1]))' $ascibinary $key) #XOR key with ascii binary of the letter
                 else
                     xor_2=$w
                 fi
-                ascichar=$(echo "$xor_2" | perl -lpe '$_=pack"B*",$_')
+                ascichar=$(echo "$xor_2" | perl -lpe '$_=pack"B*",$_') # convert from binary ascii to letter
                 echo -e -n "$ascichar" >>"$plainfile"
             else
                 continue
@@ -131,6 +135,8 @@ function decryption() {
     done <"$decFile"
 
 }
+
+clear
 
 echo "$(tput bold)$(tput setaf 2)"------------------------------------------------------""
 
